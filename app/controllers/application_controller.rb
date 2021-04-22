2d242dd85ca2534053582c245627a1c995e2ce7a
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-04-22 14:59:32 +0000

    played rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write code
+    # render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    #redirect_to("https://www.wikipedia.com") 
+
+    render({ :template => "game_templates/user_rock.html.erb"})
+
+  end
+
+
 end
